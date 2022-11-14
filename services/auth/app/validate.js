36a31d0cea0ef4e60112d2cad1dff533355acdc5
Nestor Sapuntsov <nestor.sapuntsov@Nestors-MacBook-Pro.local>
2022-11-14 15:12:18 +0100

    feat: added a new auth service

---
+++ services/auth/app/validate.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const jwt_secret = process.env.JWT_SECRET || "secret";
+
+async function validate(req, res, next) {
+    const token =
+    req.body.token || req.query.token || req.headers["x-access-token"] || req.headers.authorization;
+    if (!token)
+        return res.status(403).send({ message: "A token is required for authentication"} );
+
+    try {
+        const decoded = jwt.verify(token, jwt_secret);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).send({ message: "Invalid Token" });
+    }
+    return next();
+};
+
+module.exports = validate;

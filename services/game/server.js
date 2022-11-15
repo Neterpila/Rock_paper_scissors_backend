5a07e26eddf352e031fca9055ef3980aa7ebf9e0
Daniel_S <69630750+konannik@users.noreply.github.com>
2022-11-15 13:51:39 +0100

    Added simple working game service

--- services/game/server.js
+++ services/game/server.js
@@ -1,18 +1,40 @@
-const { Server } = require('ws');
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(3001,function() {
+    console.log('GameServer listening for requests on port 3001');
+});
  
-const port = process.env.PORT || 3001;
+app.use(express.static('app'));
 
-const sockets_server = new Server({ port: port });
+// const port = process.env.PORT || 3001;
+// const sockets_server = new Server({ port: port });
 
-sockets_server.on('connection', (ws, req) => {
-    console.log('New client connected!'); 
-    //console.log(req);
-    //console.log(sockets_server.clients);
+var io = socket(server);
+io.on('connection',(socket) => {
 
-    ws.on('message', (data) => {
-        const message = JSON.parse(data);
-        console.log(message);
+    console.log('Made socket connection',socket.id);
+
+    socket.on('communicat',function(data) {
+        io.sockets.emit('communicat',data);
     });
 
-   ws.on('close', () => console.log('Client has disconnected!'));
+    
+
 });
+
+// sockets_server.on('connection', (ws, req) => {
+//     console.log('New client connected!'); 
+//     //console.log(req);
+//     //console.log(sockets_server.clients);
+
+//     ws.on('message', (data) => {
+//         const message = JSON.parse(data);
+//         console.log(message);
+
+//     });
+
+
+//    ws.on('close', () => console.log('Client has disconnected!'));
+// });

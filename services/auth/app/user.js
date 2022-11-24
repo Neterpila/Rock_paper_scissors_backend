4fb33a43c6688839c89fac1d7a718023af0e8e50
Nestor Sapuntsov <nestor.sapuntsov@Nestors-MacBook-Pro.local>
2022-11-24 23:55:59 +0100

    refactor: changed user schema and auth endpoints
    /register and /login endpoints now return only plain jwt payload instead of complete token with a signature. The token is instead going to be signed by api gateway using the jwk exposed by auth service. Incoming tokens from clients will also be verified by gateway using the abovementioned key

--- services/auth/app/user.js
+++ services/auth/app/user.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const User = new Schema ({
-    nickname: { type: String, unique: true },
-    password: { type: String },
-    token: { type: String }
+    username: { type: String, unique: true },
+    password: { type: String }
 });
 
 module.exports = mongoose.model("User", User);

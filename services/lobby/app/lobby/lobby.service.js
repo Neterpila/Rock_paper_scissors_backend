9c048d159fa9ab65a476e1d0eb3d35d1ab138d34
Nestor Sapuntsov <nestor.sapuntsov@Nestors-MacBook-Pro.local>
2022-11-25 00:06:56 +0100

    fix: endpoints now use data from auth token to determine user, lobbies now have an owner

--- services/lobby/app/lobby/lobby.service.js
+++ services/lobby/app/lobby/lobby.service.js
@@ -1,40 +1,90 @@
 const Lobby = require('./lobby');
 const _ = require('lodash');
 
-async function create(data) {
+async function create(data, owner) {
     let lobby;
     try {
-        lobby = new Lobby(_.pick(data, ["name", "password"]));
-        lobby = await lobby.save();
+        lobby = _.pick(data, ["name", "password"]);
+        lobby.owner = owner;
+        console.log(lobby);
+        lobby = new Lobby(lobby);
+        await lobby.save();
     } catch (e) {
+        console.error(e);
         throw {
             type: "invalid_data"
         }
     }
+
     return lobby;
+}
+
+async function remove(lobby_id, owner) {
+    let lobby;
+    try {
+        lobby = await Lobby.findById(lobby_id);
+        if (!lobby)
+            throw {};
+    } catch (e) {
+        throw {
+            type: "does_not_exist"
+        }
+    }
+
+    if (lobby.owner !== owner)
+        throw {
+            type: "not_an_owner"
+        }
+
+    await Lobby.deleteOne(lobby);
+    return;
 }
 
 async function get(filters = {}) {
     return Lobby.find(filters);
 }
 
-async function join(lobby_id, user_id) {
-    let lobby = await Lobby.findById(lobby_id);
-    if (!lobby)
+async function join(lobby_id, user) {
+    let lobby;
+    try {
+        lobby = await Lobby.findById(lobby_id);
+        if (!lobby)
+            throw {};
+    } catch (e) {
         throw {
-            type: "lobby_does_not_exist"
+            type: "does_not_exist"
         }
+    }
+
+    if(lobby.players.includes(user))
+        return lobby;
 
     if (lobby.players.length >= 2)
         throw {
             type: "lobby_full"
         }
 
-    if (!lobby.players.includes(user_id)) {
-        lobby.players.push(user_id);
-        lobby = await lobby.save();
-    }
+    lobby.players.push(user);
+    lobby = await lobby.save();
     return lobby;
 }
 
-module.exports = { create, get, join };
+async function leave(lobby_id, user) {
+    let lobby;
+    try {
+        lobby = await Lobby.findById(lobby_id);
+        if (!lobby)
+            throw {};
+    } catch (e) {
+        throw {
+            type: "does_not_exist"
+        }
+    }
+
+    lobby.players = _.filter(lobby.players, player => player !== user);
+    lobby = await lobby.save();
+
+    return lobby;
+}
+
+module.exports = { create, remove, get, join, leave };

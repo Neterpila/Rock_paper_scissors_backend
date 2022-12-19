723355444e42a3353dc988144017f25bf2aa2964
Nestor Sapuntsov <nestor.sapuntsov@Nestors-MacBook-Pro.local>
2022-12-19 14:58:10 +0100

    fix: changed auth approach
    /login and /register endpoints now send signed token in their responses.
    Service no longer has endpoint for fetching key(s). Instead the key must be passed as an env.
    Service now has a new endpoint /validate that validates a token and either returns 200 with it's payload in the body, or 400 if the token is invalid. This endpoint will be used by the new gateway in the future.

--- services/auth/server.js
+++ services/auth/server.js
@@ -2,14 +2,6 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const fs = require('fs');
-const jwk_path = './config/jwk.json';
-
-if (!fs.existsSync(jwk_path))
-    throw {
-        message: "no jwk file found under " + jwk_path
-    }
-
 const db = require('./app/db');
 
 const app = express();
@@ -20,8 +12,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(require("./app/auth"));
-
-app.use("/jwk", express.static(jwk_path));
 
 app.get("/health", (req, res) => {
     return res.status(200).send({ status: "ok" });
